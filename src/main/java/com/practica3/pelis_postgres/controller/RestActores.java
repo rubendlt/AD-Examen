98a175b55e85000818c1746ea32d790c4b09393d
ruben <rubendlt2006@gmail.com>
2026-01-28 08:49:19 +0100

    Rest hechos

--- src/main/java/com/practica3/pelis_postgres/controller/RestActores.java
+++ src/main/java/com/practica3/pelis_postgres/controller/RestActores.java
@@ -1,4 +1,32 @@
 package com.practica3.pelis_postgres.controller;
 
+import com.practica3.pelis_postgres.entities.Actor;
+import com.practica3.pelis_postgres.repositories.ActorRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/actores")
 public class RestActores {
+    private final ActorRepository actorRepository;
+
+    public RestActores(ActorRepository actorRepository) {
+        this.actorRepository = actorRepository;
+    }
+
+    @GetMapping
+    public List<Actor> getActores() {
+        return actorRepository.findAll();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> borrarActor(@PathVariable Long id) {
+        if (actorRepository.existsById(id)) {
+            actorRepository.deleteById(id);
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.notFound().build();
+    }
 }
--- src/main/java/com/practica3/pelis_postgres/controller/RestPeliculas.java
+++ src/main/java/com/practica3/pelis_postgres/controller/RestPeliculas.java
@@ -1,4 +1,39 @@
 package com.practica3.pelis_postgres.controller;
 
+import com.practica3.pelis_postgres.entities.Pelicula;
+import com.practica3.pelis_postgres.repositories.PeliculaRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/peliculas")
 public class RestPeliculas {
+    private final PeliculaRepository peliculaRepository;
+
+    public RestPeliculas(PeliculaRepository peliculaRepository) {
+        this.peliculaRepository = peliculaRepository;
+    }
+
+    @GetMapping
+    public List<Pelicula> getPeliculas() {
+       return peliculaRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Pelicula> getPeliculaById(@PathVariable Long id) {
+        return peliculaRepository.findById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public ResponseEntity<Pelicula> gardarPelicula(@RequestBody Pelicula pelicula) {
+        if (pelicula.getActores() != null) {
+            pelicula.getActores().forEach(actor -> actor.setPelicula(pelicula));
+        }
+        Pelicula gardada = peliculaRepository.save(pelicula);
+        return ResponseEntity.ok(gardada);
+    }
 }
