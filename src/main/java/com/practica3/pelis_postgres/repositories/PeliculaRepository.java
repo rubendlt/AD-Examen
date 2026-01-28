a2c7e0915e7e79040f8c25c79c073217c7f00181
ruben <rubendlt2006@gmail.com>
2026-01-29 00:27:01 +0100

    Rest hechos

--- src/main/java/com/practica3/pelis_postgres/repositories/PeliculaRepository.java
+++ src/main/java/com/practica3/pelis_postgres/repositories/PeliculaRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface PeliculaRepository extends JpaRepository<Pelicula, Long> {
-    List<Pelicula> findByTiutulo(String titulo);
+    List<Pelicula> findByTitulo(String titulo);
 }
